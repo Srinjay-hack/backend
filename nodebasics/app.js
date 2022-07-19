f4d952ea09eac67cb9e7bf871d7d6bffcd61432b
Srinjay-hack <srinjaymondal@gmail.com>
2022-07-19 22:35:09 +0530

    new

--- nodebasics/app.js
+++ nodebasics/app.js
@@ -2,9 +2,15 @@ const http = require("http");
 const hostname="127.0.0.1";
 const port=3000;
 const server = http.createServer((req,res)=>{
-   res.statusCode=200;
-   res.setHeader('Content-Type','text/pain');
-   res.end('Hello World');
+   if(req.url === '/'){
+      res.end("Hello World");
+   }
+   else if(req.url === '/api'){
+      res.end("Hello API");
+   }
+   else{
+      res.end("Nothimg to show");
+   }
 });
 
 server.listen(port,hostname,()=>{
